7b91b71e3169084680b0558bb5df8ab38cb8b9c3
Jing Zheng <jingzheng2016@gmail.com>
2018-10-28 05:12:15 -0400

    add showing words' term frequency function

--- statistics_20newsgroup.py
+++ statistics_20newsgroup.py
@@ -3,11 +3,20 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import nltk
 from sklearn import datasets
-
+import re
 from statistics import mean, stdev
+from gensim.parsing.porter import PorterStemmer
+from gensim.parsing.preprocessing import remove_stopwords
 
-news_dataset = datasets.fetch_20newsgroups(subset="all", remove=("headers", "footers", "quotes"))
-documents = news_dataset.data
+
+def preprocessing_documents(documents):
+    for i in range(len(documents)):
+        documents[i] = documents[i].lower()
+        documents[i] = re.sub('[^ a-z0-9\.\?\!]+', '', documents[i])
+        documents[i] = remove_stopwords(documents[i])
+
+    return documents
+
 
 
 def get_sentences_length_words(documents):
@@ -30,6 +39,21 @@ def boxplot(sentence_length):
     plt.show()
 
 
+def plot_tf(words):
+    freqdist = nltk.FreqDist(words)
+    plt.figure(figsize=(16, 5))
+    freqdist.plot(50)
+
+def postprocess_words(words):
+    for i in range(len(words)):
+        p = PorterStemmer()
+        words[i] = p.stem(words[i])
+    return words
+
+
+news_dataset = datasets.fetch_20newsgroups(subset="all", remove=("headers", "footers", "quotes"))
+documents = preprocessing_documents(news_dataset.data)
+
 sentences_length, words = get_sentences_length_words(documents)
 min_sentence_length = min(sentences_length)
 max_sentence_length = max(sentences_length)
@@ -41,6 +65,7 @@ boxplot(sentences_length)
 n_books = len(documents)
 n_sentences = len(sentences_length)
 n_words = len(words)
+words = postprocess_words(words)
 n_unique_words = len(Counter(words))
 
 print('{0:>7} {1:>12} {2:>12} {3:>12}'.format('max', 'min', 'mean', 'std'))
@@ -48,3 +73,5 @@ print('{0:>7} {1:>12} {2:>12} {3:>12}'.format(max_sentence_length, min_sentence_
 print('\n\n\n')
 print('{0:>20} {1:>20} {2:>15} {3:>25} {4:>35}'.format('# of books', '# of sentences', '# of words', '# of unique words', 'mean # of words per sentence'))
 print('{0:>20} {1:>20} {2:>15} {3:>25} {4:>35}'.format(n_books, n_sentences, n_words, n_unique_words, int(mean_sentence_length)))
+
+plot_tf(words)
